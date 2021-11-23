34a5f1449af913f04fac0511a8ef0b66f0b0a441
Doc. Shaka <61460961+DocShaka@users.noreply.github.com>
2021-11-23 17:36:28 +0100

    Update H_B64.py

--- H_B64.py
+++ H_B64.py
@@ -1,10 +1,6 @@
-import random
-import string
-import os
 import base64
-from base64 import *
-from base64 import encode, decode, b64encode, b64decode
-from random import choice, randint
+from base64 import standard_b64decode, standard_b64encode
+from random import choice
 from art import *
 from termcolor import colored
 
@@ -17,38 +13,38 @@ def main():
 
     try:
         print("="*95)
-        print("/tbonjour, que voulez vous faire ? (Encodé/Decodé) :")
-        choice = input("\t> ")
+        print("Hacheur_Base64:$> Bonjour, que voulez vous faire ? (Encodé/Decodé) :\n")
+        choice = input("Hacheur_Base64:$>")
 
         if choice == 'E':
-            encode_message =input("Entré le texte a encodé: >")
-            encoder = base64.encode(encode_message)
-            print("[+] En Cours d'Encodage.....\n")
-            print("Encodage Terminé !\n")
+            encode_message =input("Hacheur_Base64:$> Entré le texte a encodé:>")
+            encoder = base64.standard_b64encode(encode_message)
+            print("Hacheur_Base64:$> [+] En Cours d'Encodage.....\n")
+            print("Hacheur_Base64:$> Encodage Terminé !\n")
             print(encoder)
             
             main()
         
         elif choice == 'D':
-            decode_message =input("Entré le texte a décodé: >")
-            decoder = base64.b64decode(decode_message)
-            print("[+] En Cours de Décodage.....\n")
-            print("Décodage Terminé !\n")
+            decode_message =input("Hacheur_Base64:$> Entré le texte a décodé:>")
+            decoder = base64.standard_b64decode(decode_message)
+            print("Hacheur_Base64:$> [+] En Cours de Décodage.....\n")
+            print("Hacheur_Base64:$> Décodage Terminé !\n")
             print(decoder)
 
             main()
         
-        print("\tVoulez vous Retourné à l'acceuil ? (y/n)")
+        print("Hacheur_Base64:$> Voulez vous Retourné à l'acceuil ? (y/n)")
         choice = input("\t> ")
 
         if choice == 'y':
             main()
         
         elif choice == 'n':
-            print("\t[!] Au Revoir !!")
+            print("Hacheur_Base64:$> [!] Au Revoir !!")
 
 
     except KeyboardInterrupt:
-        print("\n\t[!] Vous avez appuyé sur ctrl + C pour quitter instantanément")
+        print("Hacheur_Base64:$> [!] Vous avez appuyé sur ctrl + C pour quitter instantanément !!")
 
 main()

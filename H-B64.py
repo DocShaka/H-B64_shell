3004c7f3cf81a2d677654129117239f793d15796
Doc. Shaka <61460961+DocShaka@users.noreply.github.com>
2021-11-23 17:57:07 +0100

    Update H-B64.py

--- H-B64.py
+++ H-B64.py
@@ -16,38 +16,38 @@ def main():
 
     try:
         print("="*95)
-        print("Hacheur_Base64:$> Bonjour, que voulez vous faire ? (Encodé/Decodé) :\n")
-        choice = input("Hacheur_Base64:$>")
+        print("H-B64:$> Bonjour, que voulez vous faire ? (Encodé/Decodé) :\n")
+        choice = input("H-B64:$>")
 
         if choice == 'E':
             encode_message =input("Hacheur_Base64:$> Entré le texte a encodé:>")
             encoder = base64.standard_b64encode(encode_message)
-            print("Hacheur_Base64:$> [+] En Cours d'Encodage.....\n")
-            print("Hacheur_Base64:$> Encodage Terminé !\n")
+            print("H-B64:$> [+] En Cours d'Encodage.....\n")
+            print("H-B64:$> Encodage Terminé !\n")
             print(encoder)
             
             main()
         
         elif choice == 'D':
-            decode_message =input("Hacheur_Base64:$> Entré le texte a décodé:>")
+            decode_message =input("H-B64:$> Entré le texte a décodé:>")
             decoder = base64.standard_b64decode(decode_message)
-            print("Hacheur_Base64:$> [+] En Cours de Décodage.....\n")
-            print("Hacheur_Base64:$> Décodage Terminé !\n")
+            print("H-B64:$> [+] En Cours de Décodage.....\n")
+            print("H-B64:$> Décodage Terminé !\n")
             print(decoder)
 
             main()
         
-        print("Hacheur_Base64:$> Voulez vous Retourné à l'acceuil ? (y/n)")
+        print("H-B64:$> Voulez vous Retourné à l'acceuil ? (y/n)")
         choice = input("\t> ")
 
         if choice == 'y':
             main()
         
         elif choice == 'n':
-            print("Hacheur_Base64:$> [!] Au Revoir !!")
+            print("H-B64:$> [!] Au Revoir !!")
 
 
     except KeyboardInterrupt:
-        print("Hacheur_Base64:$> [!] Vous avez appuyé sur ctrl + C pour quitter instantanément !!")
+        print("H-B64:$> [!] Vous avez appuyé sur ctrl + C pour quitter instantanément !!")
 
 main()
